ecdf0c53845df8e3e473675979308d73b9452abe
pablo@panelalaptop <pablo.adames@proton.me>
2026-03-02 00:26:36 -0700

    fix(zero-lenght) dealing with zero length sample vectors

--- R/tstatistic.R
+++ R/tstatistic.R
@@ -1,7 +1,14 @@
 # the st
 tstatistics=function(x,y) {
-  m  <- length(x)
-  n  <- length(y)
+  m <- length(x)
+  n <- length(y)
+  m  <- ifelse(m>0, m, NA)
+  n  <- ifelse(n>0, n, NA)
+
+  if( any(is.na(c(m,n)))) {
+    return (c(m,n))
+  }
+  
   sp <- sqrt(((m-1)*sd(x)^2 + (n-1)*sd(y)^2)/(m+n-2))
   (mean(x)-mean(y))/(sp*sqrt(1/m+1/n))
 }
--- tests/testthat/test-tstatistic.R
+++ tests/testthat/test-tstatistic.R
@@ -5,7 +5,7 @@ source(here::here("R","tstatistic.R"))
 #the following only works when running testthat
 # source("../../R/tstatistic.R")
 
-test_that("t-statistic properly computed",
+test_that("t-statistics properly computed",
           {
             x <- c(34,2,-6,30,92,-46,10,31,56)
             y <- c(36,-29, 45,29,67,79,-26,0,10,11)
@@ -16,4 +16,15 @@ test_that("t-statistic properly computed",
             
             expect_equal(calc, tstatistics(x,y))
             
+          })
+
+
+test_that("t-statistics return vector with NAs for any zero-length sample",
+          {
+            x <- c()
+            y <- c(1,2,3,4,5)
+
+            expect_true(any(is.na(tstatistics(x,y))))
+            expect_equal(c(NA,length(y)), tstatistics(x,y))
+            expect_equal(c(length(y),NA), tstatistics(y,x))
           })
